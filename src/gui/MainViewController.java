e3b533599eb7a86e26d99afae226a0228d1b0da3
Jean Pierre <tavares1988@live.com>
2020-04-02 18:00:15 -0300

    About view implementation

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable{
 
@@ -30,7 +39,7 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/About.fxml");
 	}
 	
 	
@@ -40,4 +49,40 @@ public class MainViewController implements Initializable{
 		
 	}
 
+	// synchronized -> para que todo o processo seja executado sem interrupção durante multi-thread
+	private synchronized void loadView(String absoluteView) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteView));
+			VBox newVBox = loader.load();
+			
+			// Pega referência do painel principal (referência para cena)
+			Scene mainScene = Main.getMainScene();
+			
+			// Pega referência do VBox do painel principal
+			/*
+			 ** getRoot() --> pega o primeiro elemnto na referência do painel principal que é um scrollPane
+			 ** ((ScrollPane)mainScene.getRoot()) é uma referência para o ScrollPane
+			 ** getContent() --> pega o content dentro do ScrollPane que já é uma referência para o VBox
+			 ** dentro do ScrollPane
+			 */
+			VBox mainVBox = (VBox)((ScrollPane)mainScene.getRoot()).getContent();
+			
+			/*
+			 * getChildren().get(0) --> pega o filho do VBox na posição 0 (zero) (a barra de menu)
+			*/
+			Node mainMenu = mainVBox.getChildren().get(0);
+			
+			/*Limpa todos os filhos do mainVBox*/
+			mainVBox.getChildren().clear();
+			
+			/*Adicionando filhos ao mainVBox*/
+			mainVBox.getChildren().add(mainMenu);
+			
+			/*Adicionando uma coleção de filhos ao mainVBox*/
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
 }
